feac4c7de5d25007e4945a2c9c55f6440f87473c
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2025-06-16 22:10:51 -0300

    feat: add user class

---
+++ src/main/java/com/samanthamaiaduarte/todoapi/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.samanthamaiaduarte.todoapi.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String login;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public User (String login, String password, UserRole role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+}
