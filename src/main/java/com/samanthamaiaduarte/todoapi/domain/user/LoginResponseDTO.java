5c6116ca6814bb7f0a2bca464ed01e194b5c2122
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2025-06-17 17:22:01 -0300

    feat: changing token response

--- src/main/java/com/samanthamaiaduarte/todoapi/domain/user/LoginResponseDTO.java
+++ src/main/java/com/samanthamaiaduarte/todoapi/domain/user/LoginResponseDTO.java
@@ -1,4 +1,6 @@
 package com.samanthamaiaduarte.todoapi.domain.user;
 
-public record LoginResponseDTO(String token) {
+import java.time.LocalDateTime;
+
+public record LoginResponseDTO(LocalDateTime refresh, String token_type, String access_token, Integer expires_in) {
 }
